41053cdfa69309eb7ce40065a014afef2ab54402
Dragonath <dragonathplays@gmail.com>
2025-05-22 13:50:30 +0300

    QTE + tilemap and background

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
     public static PlayerScript instance; // Singleton instance
     public bool playerHasControl = true; // Boolean to check if the player has control
 
+    private InputAction moveAction;
+    private InputAction jumpAction;
+    private InputAction interactionAction;
+    private InputAction pauseAction;
 
     private void Awake()
     {
@@ -20,12 +25,15 @@ public class PlayerScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+        interactionAction = InputSystem.actions.FindAction("Interact");
+        pauseAction = InputSystem.actions.FindAction("Menu");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
