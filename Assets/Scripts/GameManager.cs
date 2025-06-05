86076395c222b1d78673feaa0f0b0666e7a1fa73
Samuli Multimäki <samuli.multimaki21@gmail.com>
2025-06-05 10:15:42 +0300

    Dialogue systeemi tehty toimimaan

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -46,7 +46,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
         if (speedrunMode || clock.GetComponent<Clock>().current)
